b19240d457e6df33f35c9165c604f7e4db8f9ecd
woaaue <safat200@mail.ru>
2024-08-03 15:14:46 +0300

    minor edit tween

--- Assets/Scripts/UI/Banknote/BanknoteAnimation.cs
+++ Assets/Scripts/UI/Banknote/BanknoteAnimation.cs
@@ -14,7 +14,7 @@ public sealed class BanknoteAnimation : MonoBehaviour
         _sequence
             .Append(transform.DOLocalJump(directionX, 150f, 1, 1.5f).SetEase(Ease.OutQuad))
             .Join(transform.DORotate(new Vector3(0, 0, UnityEngine.Random.Range(360, 720)), 1.5f, RotateMode.FastBeyond360))
-            .Join(transform.DOLocalMoveY(-Screen.height - 150, 3f).SetEase(Ease.InQuad).SetDelay(1f))
+            .Join(transform.DOLocalMoveY(-Screen.height - 150, 2f).SetEase(Ease.InQuad).SetDelay(1f))
             .OnComplete(() =>
             {
                 SequenceCompleted?.Invoke();
